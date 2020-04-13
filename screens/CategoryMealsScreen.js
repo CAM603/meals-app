27165440af30db7f7a191303bd1e82e9fcd96bd2
CAM603 <cameronhawley@comcast.net>
2020-04-13 09:39:17 -0400

    ADD mealItem

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,38 +1,56 @@
-import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
-import { CATEGORIES } from '../data/dummy-data';
+import React from "react";
+import { View, Text, StyleSheet, Button, FlatList } from "react-native";
+import { CATEGORIES, MEALS } from "../data/dummy-data";
+import MealItem from "../components/MealItem";
 
 const CategoryMealsScreen = (props) => {
-  const catId = props.navigation.getParam('categoryId');
+  const catId = props.navigation.getParam("categoryId");
 
-  const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
+  const displayedMeals = MEALS.filter(
+    (meal) => meal.categoryIds.indexOf(catId) >= 0
+  );
+
+  const renderMealItem = (itemData) => {
+    return (
+      <MealItem
+        title={itemData.item.title}
+        duration={itemData.item.duration}
+        complexity={itemData.item.complexity}
+        affordability={itemData.item.affordability}
+        image={itemData.item.imageUrl}
+        onSelectMeal={() => {}}
+      />
+    );
+  };
 
   return (
     <View style={styles.screen}>
-      <Text>{selectedCategory.title}</Text>
-      <Button title="Go To Meal Details" onPress={() => {
-        props.navigation.navigate('MealDetail')
-      }}/>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item, index) => item.id}
+        renderItem={renderMealItem}
+        style={{ width: "100%" }}
+      />
     </View>
   );
 };
 
 CategoryMealsScreen.navigationOptions = (navigationData) => {
-  const catId = navigationData.navigation.getParam('categoryId');
+  const catId = navigationData.navigation.getParam("categoryId");
 
-  const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
+  const selectedCategory = CATEGORIES.find((cat) => cat.id === catId);
 
   return {
     headerTitle: selectedCategory.title,
-  }
-}
+  };
+};
 
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
+    justifyContent: "center",
+    alignItems: "center",
+  },
 });
 
 export default CategoryMealsScreen;
